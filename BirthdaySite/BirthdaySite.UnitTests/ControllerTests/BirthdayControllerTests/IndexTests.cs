c4259f5254d2e8c88ca88c67ee77c55e3c28f76a
iliyaST <iliamstambolov@gmail.com>
2017-10-11 12:55:58 +0300

    Added IndexTests.cs

---
+++ BirthdaySite/BirthdaySite.UnitTests/ControllerTests/BirthdayControllerTests/IndexTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BirthdaySite.UnitTests.ControllerTests.BirthdayControllerTests
+{
+    [TestClass]
+    public class IndexTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
